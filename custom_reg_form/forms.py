1d1778cbaed7108fb4373652a8221fb94dae9b08
A K <admin@As-MacBook-Pro.local>
2020-09-16 11:52:48 +0200

    Added employment_status_choices and email_gdpr_consent as ExtraInfo options.

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -7,11 +7,11 @@ class ExtraInfoForm(ModelForm):
     """
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields['favorite_movie'].error_messages = {
-            "required": u"Please tell us your favorite movie.",
-            "invalid": u"We're pretty sure you made that movie up.",
-        }
+        self.fields['employment_status'].error_messages = {
+            "required": u"Please tell us your employment status",
+            "invalid": u"Enter correct employment status",
+            },
 
     class Meta(object):
         model = ExtraInfo
-        fields = ('favorite_editor', 'favorite_movie')
+        fields = ('employment_status','email_gdpr_consent',)
--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils.translation import ugettext_noop
 
 # Backwards compatible settings.AUTH_USER_MODEL
 USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
@@ -10,21 +11,29 @@ class ExtraInfo(models.Model):
     This model contains two extra fields that will be saved when a user registers.
     The form that wraps this model is in the forms.py file.
     """
-    user = models.OneToOneField(USER_MODEL, null=True, on_delete=models.CASCADE)
-    FAVORITE_EDITOR = (
-        ('vim', 'Vim'),
-        ('emacs', 'Emacs'),
-        ('np', 'Notepad'),
-        ('cat', 'cat > filename'),
+    user = models.OneToOneField(USER_MODEL, null=True)
+    EMPLOYMENT_STATUS_CHOICES = (
+        ('efw', ugettext_noop('Employed for wages')),
+        ('selfemployed', ugettext_noop('Self-employed')),
+        ('student', ugettext_noop('Student')),
+        ('homemaker', ugettext_noop('Homemaker')),
+        ('oowlfw', ugettext_noop('Out of work and looking for work')),
+        ('oownclfw', ugettext_noop('Out of work but not currently looking for work')),
+        ('military', ugettext_noop('Military')),
+        ('retired', ugettext_noop('Retired')),
+        ('utw', ugettext_noop('Unable to work')),
+        ('other', ugettext_noop('Other')),
     )
-
-    favorite_movie = models.CharField(
-        verbose_name="Fav Flick",
-        max_length=100,
+    employment_status = models.CharField(
+        verbose_name="Employment Status",
+        blank=True, null=True, max_length=20, db_index=True,
+        choices=EMPLOYMENT_STATUS_CHOICES
     )
-    favorite_editor = models.CharField(
-        verbose_name="Favorite Editor",
-        choices=FAVORITE_EDITOR,
-        blank=True,
-        max_length=5,
+    user = models.BooleanField(USER_MODEL,default=True)
+    EMAIL_GDPR_CONSENT = (
+    )
+    email_gdpr_consent = models.BooleanField(
+        verbose_name="Subscribe me to the newsletter from NTNU Videre",
+        blank=True, db_index=True, default=False,
+        choices=EMAIL_GDPR_CONSENT
     )
