ce602bd4eaf4e5e2b7d2eaee7374d283f262a5f9
murat-polat <muratsdepo@gmail.com>
2020-09-17 07:26:35 +0000

    checkbox,felt virker

--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -8,11 +8,8 @@ USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class ExtraInfo(models.Model):
-
-
     user = models.OneToOneField(USER_MODEL, null=True, on_delete=models.CASCADE)
     samtykke = models.BooleanField(default=True)
     #baretest = models.TextField()
-    VALG = [('JA','Samtykker'),
-           (('NEI','Samtykker IKKE'),]
+    VALG = (('JA','Samtykker'),('NEI','Samtykker IKKE'),)
     velge = models.CharField(blank=False,verbose_name='Jeg godtar NTNUs betingelser om marketsforing ', choices=VALG, max_length=64)
