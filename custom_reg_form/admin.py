3baa1bb138e26768d5fc64564ffc456e553cf474
murat-polat <muratsdepo@gmail.com>
2020-09-21 16:19:40 +0200

    better admin side

--- custom_reg_form/admin.py
+++ custom_reg_form/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import ExtraInfo
 
-admin.site.register(ExtraInfo)
+class ExtraInfoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email', 'samtykke', 'employment_status',)
+    list_display_links = ('user', 'email','employment_status',)
+    list_filter = ('user',)
+    search_fields = ('user', 'email', 'samtykke', 'employment_status',)
+    list_per_page = 25
+
+
+admin.site.register(ExtraInfo, ExtraInfoAdmin)
