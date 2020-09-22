0ab5b36ac40703039ce1382eda2c524df6aa170d
murat-polat <muratsdepo@gmail.com>
2020-09-22 14:34:34 +0200

    DowloadCSV works

--- custom_reg_form/admin.py
+++ custom_reg_form/admin.py
@@ -5,30 +5,30 @@ from django.http import HttpResponse
 from io import StringIO
 
 
-def download_csv(modeladmin, request, queryset):
-    import csv
-    from django.http import HttpResponse
-    from io import  StringIO
-    f = StringIO()
-    writer = csv.writer(f)
-    writer.writerow(["user", "email", "samtykke", "employment_status"])
-    for s in queryset:
-        writer.writerow([s.user, s.email, s.samtykke, s.employment_status])
-    f.seek(0)
-    response = HttpResponse(f, content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=extra-info.csv'
-    return response    
-
 class ExtraInfoAdmin(admin.ModelAdmin):  
-    actions = ['download_csv']  
+    actions = ['download_csv'] 
     list_display = ('user', 'email', 'samtykke', 'employment_status',)
     list_display_links = ('user', 'email','employment_status',)
     list_filter = ('user',)
     search_fields = ('user', 'email', 'samtykke', 'employment_status',)
     list_per_page = 25  
-    def download_csv(self,modeladmin, request, queryset):
-        None  
-    download_csv.short_description = "Last ned som CSV."
+    def download_csv(self, request, queryset,*args, **kwargs):
+        import csv
+        f = open('some.csv', 'w')
+        writer = csv.writer(f)
+        writer.writerow(["user", "email", "employment_status"])
+        for s in queryset:
+            writer.writerow([s.user, s.email, s.employment_status])
+        
+        f.close()
+        f = open('some.csv', 'r')
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=stat-info.csv'
+        return response    
+    download_csv.short_description = "Download CSV "
 
-    
+
+
 admin.site.register(ExtraInfo, ExtraInfoAdmin)
+
+
